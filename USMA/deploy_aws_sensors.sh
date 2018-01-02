e673ad552bf6e7c1157787c33680a4b18ccd3529
thesraid <thesraid@gmail.com>
2018-01-02 13:41:12 +0000

    Create deploy_aws_sensors.sh

---
+++ USMA/deploy_aws_sensors.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+wget https://s3.amazonaws.com/downloads.alienvault.cloud/usm-anywhere/sensor-images/usm-anywhere-sensor-aws-vpc.template
+x=1
+while [ $x -le $1 ]
+do
+  #touch "${x}_file"
+  aws cloudformation deploy --template-file usm-anywhere-sensor-aws-vpc.template --stack-name "autodeploy${x}" --parameter-overrides KeyName=SKO2018 VpcId=vpc-a07eaec7 NodeName="autodeploy${x}" SubnetId=subnet-974bb8f0 --capabilities CAPABILITY_IAM
+  x=$(( $x + 1 ))
+done
