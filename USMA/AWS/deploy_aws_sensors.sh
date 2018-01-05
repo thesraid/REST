2a388d21345b0bbfd44573199b0b3486de69c32b
thesraid <thesraid@gmail.com>
2018-01-05 16:37:19 +0000

    Update deploy_aws_sensors.sh

--- USMA/AWS/deploy_aws_sensors.sh
+++ USMA/AWS/deploy_aws_sensors.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Still very rough. Should find a way to ensure sensor is up before trying to connect 
+# Should move to Python
+
 if [ -z "$1" ] || [ -z "$2" ]; then
    echo "USAGE:  ./deploy_aws_sensors.sh [num_of_sensors] [sensor_name]"
 exit
@@ -8,7 +11,13 @@ wget -q https://s3.amazonaws.com/downloads.alienvault.cloud/usm-anywhere/sensor-
 x=1
 while [ $x -le $1 ]
 do
-  #echo "${x}_${2}"
-  aws cloudformation deploy --template-file usm-anywhere-sensor-aws-vpc.template --stack-name "${2}-${x}" --parameter-overrides KeyName=SKO2018 VpcId=vpc-a07eaec7 NodeName="${2}-${x}" SubnetId=subnet-974bb8f0 --capabilities CAPABILITY_IAM
+  #aws cloudformation deploy --template-file usm-anywhere-sensor-aws-vpc.template --stack-name "${2}-${x}" --parameter-overrides KeyName=JohnO VpcId=vpc-198db97d NodeName="${2}-${x}" SubnetId=subnet-426c2a1a --capabilities CAPABILITY_IAM
+  sleep 5
+  echo "The instance has been deployed and it's IP address is"
+  ip=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${2}-${x}" --query "Reservations[*].Instances[*].NetworkInterfaces[*].Association.PublicIp" --output text)
+  echo $ip
+  echo "Giving the sensor a minute to fully boot"
+  sleep 60
+  ./add_sensor.py -u USM-Anywhere-Training@alienvault.com -p Password1! -d training-team-longterm-1-20171003.alienvault.cloud -s $ip
   x=$(( $x + 1 ))
 done
