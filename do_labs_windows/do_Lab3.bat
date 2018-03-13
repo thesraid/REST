0566960c281591acc768c878a09b046f7b5d7df9
thesraid <thesraid@gmail.com>
2018-03-13 11:40:55 +0000

    Update do_Lab3.bat

--- do_labs_windows/do_Lab3.bat
+++ do_labs_windows/do_Lab3.bat
@@ -1,9 +1,9 @@
 @echo off
 REM joriordan@alienvault.com
 echo We will perform a bruteforce attach
-REM plink.exe root@192.168.2.15 -pw Password1! /root/scripts/SSH_Overload.sh
+plink.exe root@192.168.2.15 -pw Password1! /root/scripts/SSH_Overload.sh
 REM timeout /t 10
-REM plink.exe root@192.168.250.13 -pw Password1! /root/scripts/Malicious_Scan.sh
+plink.exe root@192.168.250.13 -pw Password1! /root/scripts/Malicious_Scan.sh
 REM timeout /t 10
 plink.exe root@192.168.2.15 -pw Password1! /usr/bin/nikto -h 192.168.250.14
 REM timeout /t 10
