0b46107956880b019a4d50af446027a0da4dabce
thesraid <thesraid@gmail.com>
2018-03-30 11:22:47 +0100

    Update do_Lab5.bat

--- do_labs_windows/do_Lab5.bat
+++ do_labs_windows/do_Lab5.bat
@@ -1,5 +1,7 @@
 @echo off
 REM joriordan@alienvault.com
+echo Enter the Fully Qualified Domain name (not URL)
+echo Example: training-eu-1-20170101.alienvault.cloud
 set /p domain=Enter Domain: 
 plink.exe root@192.168.250.13 -pw Password1! /root/scripts/resetScripts/analyst_lab5_ex2.py -d %domain% -u usm-anywhere-training@alienvault.com -p Password1!
 timeout /t 5
