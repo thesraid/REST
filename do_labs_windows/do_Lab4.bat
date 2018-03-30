036e143f32e7abfe074b7b3cef9256ad1f3b708a
thesraid <thesraid@gmail.com>
2018-03-30 11:14:40 +0100

    Update do_Lab4.bat

--- do_labs_windows/do_Lab4.bat
+++ do_labs_windows/do_Lab4.bat
@@ -8,10 +8,7 @@ START C:\Users\Administrator\Downloads\award.pdf.exe
 echo Defacing website
 timeout /t 10
 (echo ^<html^>
-echo ^<\!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"^>
-echo ^<html xmlns="http://www.w3.org/1999/xhtml"^>
 echo ^<head^>
-echo ^<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" /^>
 echo ^<title^>AlienVault Lab Test Homepage^</title^>
 echo ^</head^>
 echo ^<body^>
