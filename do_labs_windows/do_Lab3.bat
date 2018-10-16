1cb498f9936c25e4632a18ee545f124d405b4162
thesraid <thesraid@gmail.com>
2018-10-16 13:23:06 +0100

    Update do_Lab3.bat

--- do_labs_windows/do_Lab3.bat
+++ do_labs_windows/do_Lab3.bat
@@ -1,9 +1,5 @@
 @echo off
 REM joriordan@alienvault.com
-echo In order for this script to work you need to PuTTY to Linux and Kali at least one
-echo Use PuTTY on the Desktop to connect to Linux and Kali now.
-echo You do not need to log in. You can immeditely close the windows
-pause
 echo We will perform a bruteforce attack
 plink.exe root@192.168.2.15 -pw Password1! /root/scripts/SSH_Overload.sh
 timeout /t 10
