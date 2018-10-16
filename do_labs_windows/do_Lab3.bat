d72e2f3c09f1e19bf1df891dd82fa6b6cffad2c6
thesraid <thesraid@gmail.com>
2018-10-16 12:02:34 +0100

    Update do_Lab3.bat

--- do_labs_windows/do_Lab3.bat
+++ do_labs_windows/do_Lab3.bat
@@ -1,6 +1,10 @@
 @echo off
 REM joriordan@alienvault.com
-echo We will perform a bruteforce attach
+echo In order for this script to work you need to PuTTY to Linux and Kali at least one
+echo Use PuTTY on the Desktop to connect to Linux and Kali now.
+echo You do not need to log in. You can immeditely close the windows
+pause
+echo We will perform a bruteforce attack
 plink.exe root@192.168.2.15 -pw Password1! /root/scripts/SSH_Overload.sh
 timeout /t 10
 plink.exe root@192.168.250.13 -pw Password1! /root/scripts/Malicious_Scan.sh
